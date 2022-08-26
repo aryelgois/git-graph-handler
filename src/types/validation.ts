1475f9fbbe4aa51d7dd7008b0613153ca9d09e9b
Aryel Mota Góis <aryel.gois@gmail.com>
2022-08-25 23:24:50 -0300

    Add interfaces to extend JTD Schema

---
+++ src/types/validation.ts
@@ -0,0 +1,11 @@
+import { JTDSchemaType, ValidateFunction } from 'ajv/dist/jtd'
+
+export interface JTDSchemaOptions<J, T> {
+  readonly schema: JTDSchemaType<J>
+  parse: (data: J) => T
+  serialize?: (data: T) => J
+}
+
+export type JTDSchemaValidaton<J, T> = JTDSchemaOptions<J, T> & {
+  validate: ValidateFunction<J>
+}
