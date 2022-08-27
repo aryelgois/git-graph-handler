0c45aa48502a1d4d3a320e3c279f8755531de675
Aryel Mota Góis <aryel.gois@gmail.com>
2022-08-27 14:00:27 -0300

    Add `DecodedReplacerQueryRecord` interface

--- src/types/ReplacerQuery.spec.ts
+++ src/types/ReplacerQuery.spec.ts
@@ -1,9 +1,81 @@
+import { decodeBase64 } from '~/utils'
+
 import { replacerConfigRecordJTD } from './ReplacerConfig'
-import { replacerQueryRecordJTD } from './ReplacerQuery'
+
+import {
+  encodedReplacerQueryRecordJTD,
+  replacerQueryRecordJTD,
+} from './ReplacerQuery'
+
+jest.mock('~/utils')
+const mockedBase64Decoder = jest.mocked(decodeBase64)
 
 jest.mock('./ReplacerConfig')
-
 const mockedConfigParser = jest.mocked(replacerConfigRecordJTD.parse)
+
+describe('encodedReplacerQueryRecordJTD', () => {
+  describe('parse()', () => {
+    const { parse } = encodedReplacerQueryRecordJTD
+
+    it('throws when "q" is empty', () => {
+      expect(() => parse({ q: '', c: '' })).toThrowError(/Missing "q"/u)
+      expect(mockedBase64Decoder).not.toBeCalled()
+    })
+
+    it('throws when "c" is empty', () => {
+      expect(() => parse({ q: 'x', c: '' })).toThrowError(/Missing "c"/u)
+      expect(mockedBase64Decoder).not.toBeCalled()
+    })
+
+    it('throws when "c" is invalid', () => {
+      const samples: [input: string, output: string][] = [
+        ['ew==', '{'],
+        ['Ww==', '['],
+        ['eydmb28nOiAnYmFyJ30=', "{'foo': 'bar'}"],
+        ['W3sgbnVsbCB9XQ==', '[{ null }]'],
+
+        ['IiI=', '""'],
+        ['dHJ1ZQ==', 'true'],
+        ['e30=', '{}'],
+        ['eyJmb28iOiAiYmFyIn0=', '{"foo": "bar"}'],
+      ]
+
+      // eslint-disable-next-line no-magic-numbers
+      expect.assertions(samples.length * 2)
+
+      for (const [input, output] of samples) {
+        mockedBase64Decoder.mockReturnValueOnce(output)
+        expect(() => parse({ q: 'x', c: input })).toThrow(SyntaxError)
+        expect(mockedBase64Decoder).toHaveBeenLastCalledWith(input)
+      }
+    })
+
+    it('decodes the "c" property', () => {
+      const samples: [input: string, output: string, final: unknown[]][] = [
+        ['W10=', '[]', []],
+        ['W3t9XQ==', '[{}]', [{}]],
+        ['WyJzdHJpbmciXQ==', '["string"]', ['string']],
+        ['W3siZm9vIjogImJhciJ9XQ==', '[{"foo": "bar"}]', [{ foo: 'bar' }]],
+
+        [
+          // actual: [{ "in": "!(\\d+)", "to": "https://example/issues/$1" }]
+          'W3sgImluIjogIiEoXFxkKykiLCAidG8iOiAiaHR0cHM6Ly9leGFtcGxlL2lzc3Vlcy8kMSIgfV0=',
+          '[{ "in": "!(\\\\d+)", "to": "https://example/issues/$1" }]',
+          [{ in: '!(\\d+)', to: 'https://example/issues/$1' }],
+        ],
+      ]
+
+      // eslint-disable-next-line no-magic-numbers
+      expect.assertions(samples.length * 2)
+
+      for (const [input, output, final] of samples) {
+        mockedBase64Decoder.mockReturnValueOnce(output)
+        expect(parse({ q: 'x', c: input })).toEqual({ q: 'x', c: final })
+        expect(mockedBase64Decoder).toHaveBeenLastCalledWith(input)
+      }
+    })
+  })
+})
 
 describe('replacerQueryRecordJTD', () => {
   describe('parse()', () => {
--- src/types/ReplacerQuery.ts
+++ src/types/ReplacerQuery.ts
@@ -1,3 +1,7 @@
+import assert from 'assert'
+
+import { decodeBase64 } from '~/utils'
+
 import {
   replacerConfigRecordJTD,
   ReplacerConfig,
@@ -5,6 +9,48 @@ import {
 } from './ReplacerConfig'
 
 import { JTDSchemaOptions } from './validation'
+
+export interface EncodedReplacerQueryRecord {
+  q: string
+  c: string
+}
+
+export interface DecodedReplacerQueryRecord {
+  q: string
+  c: unknown[]
+}
+
+export const encodedReplacerQueryRecordJTD: JTDSchemaOptions<
+  EncodedReplacerQueryRecord,
+  DecodedReplacerQueryRecord
+> = {
+  schema: {
+    properties: {
+      q: { type: 'string' },
+      c: { type: 'string' },
+    },
+  },
+
+  parse(data) {
+    assert(data.q !== '', 'Missing "q" property')
+    assert(data.c !== '', 'Missing "c" property')
+
+    let config
+    try {
+      config = JSON.parse(decodeBase64(data.c))
+      assert(Array.isArray(config))
+    } catch (_) {
+      throw new SyntaxError(
+        'The "c" property must be a JSON array encoded in Base64'
+      )
+    }
+
+    return {
+      ...data,
+      c: config,
+    }
+  },
+}
 
 interface ReplacerQueryRecord {
   q: string
