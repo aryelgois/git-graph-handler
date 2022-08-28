b78a391df8c9f9642f61bb04024f928573bcd84a
Aryel Mota Góis <aryel.gois@gmail.com>
2022-08-28 20:09:16 -0300

    Add implementation for `GET /` endpoint

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,25 @@
 import express from 'express'
 
+import { applyReplacer, parseQuery } from '~/replacer'
+
+import { asError } from '~/utils'
+
 const app = express()
 
-app.get('/', (_req, res) => {
-  res.json({ helloWorld: 'It Works' })
+const HTTP_BAD_REQUEST = 400
+
+app.get('/', (req, res) => {
+  try {
+    const query = parseQuery(req.query)
+    const result = applyReplacer(query)
+    res.redirect(result)
+  } catch (e) {
+    const error = asError(e)
+    res.status(HTTP_BAD_REQUEST).json({
+      error: error.name,
+      message: error.message,
+    })
+  }
 })
 
 export default app
